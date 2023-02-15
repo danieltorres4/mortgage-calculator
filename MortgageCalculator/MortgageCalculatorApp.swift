413a06ebf672d162db684ccd776d095482ddba45
danieltorres4 <dst990502@outlook.com>
2023-02-15 15:23:53 -0600

    Initial Commit

---
+++ MortgageCalculator/MortgageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortgageCalculatorApp.swift
+//  MortgageCalculator
+//
+//  Created by Iván Sánchez Torres on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MortgageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
